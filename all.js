12d58a6cdc4de503110bd71d2b826908dac99cbc
unknown <asadalgalib9@gmail.com>
2024-09-11 23:20:05 +0600

    fourr

--- all.js
+++ all.js
@@ -43,3 +43,42 @@ function checkDigitsInName(name) {
     return result;
     }
 }
+
+
+function calculateFinalScore(obj) {
+    if(typeof(obj) !== "object"){
+        let result = "Invalid Input";
+        return result;
+    }
+    else{
+        if((typeof(obj.name) === "string") && (typeof(obj.testScore) === "number" && obj.testScore <= 50) && 
+        (typeof(obj.schoolGrade) === "number" && obj.schoolGrade <= 30) && (typeof(obj.isFFamily) === "boolean")){
+            if(obj.isFFamily === true){
+                let sum = obj.testScore + obj.schoolGrade + 20; 
+                if(sum >= 80){
+                    result = true;
+                    return result;
+                }
+                else{
+                    result = false;
+                    return result;
+                }
+            }
+            else{
+                let sum = obj.testScore + obj.schoolGrade; 
+                if(sum >= 80){
+                    result = true;
+                    return result;
+                }
+                else{
+                    result = false;
+                    return result;
+                }
+            }
+        }
+        else{
+            result = false;
+            return result;
+        }
+    }
+}
